ee2a19b3538997dd71a394afbf1cf7c7c01aca33
Anssi Herranen <anssi.herranen@iki.fi>
2019-03-03 21:29:47 +0200

    Added simple error handling

--- src/functions/content.ts
+++ src/functions/content.ts
@@ -1,16 +1,21 @@
-import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyHandler } from 'aws-lambda'
 import { fetchMainPage } from '@services/contentful'
+import { createErrorResponse } from '@utils/lambda'
 import Logger from '@utils/logger'
 import { middify } from '@utils/middy'
 
 const { log } = new Logger('mainPage')
 
 const mainPageHandler: APIGatewayProxyHandler = async (event, context) => {
-  const { pageName } = event.pathParameters
-  const page = await fetchMainPage(pageName as any)
-  const { fields } = page
-  log(`Successfully fetched page: ${fields.title}`)
-  return { statusCode: 200, body: JSON.stringify(fields) }
+  try {
+    const { pageName } = event.pathParameters
+    const page = await fetchMainPage(pageName as any)
+    const { fields } = page
+    log(`Successfully fetched page: ${fields.title}`)
+    return { statusCode: 200, body: JSON.stringify(fields) }
+  } catch (e) {
+    return createErrorResponse(e)
+  }
 }
 
 export const mainPage = middify(mainPageHandler)

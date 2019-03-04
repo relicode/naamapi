1afc04707a53fd26d0c4408dfaa3cfc7219299e0
Anssi Herranen <anssi.herranen@vincit.fi>
2019-03-04 17:16:18 +0200

    Removed httpErrorHandler due to bug in async functionality

--- src/utils/middy.ts
+++ src/utils/middy.ts
@@ -1,15 +1,15 @@
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 import middy, { Middy } from 'middy'
 
-// Require used because of bug in package
-const { httpSecurityHeaders, httpErrorHandler, httpEventNormalizer, jsonBodyParser } = require('middy/middlewares')
+// Require used because of bug in package exports
+// tslint:disable-next-line
+const { httpSecurityHeaders, httpEventNormalizer, jsonBodyParser } = require('middy/middlewares')
 const { stage } = process.env
 
 export const middify = (handler: APIGatewayProxyHandler): Middy<APIGatewayProxyEvent, APIGatewayProxyResult> => {
   const middified = middy(handler)
     .use(jsonBodyParser())      // parses the request body when it's a JSON and converts it to an object
-    .use(httpErrorHandler())    // handles common http errors and returns proper responses
-    .use(httpEventNormalizer()) // queryStringParameters and pathParameters always available
+    .use(httpEventNormalizer()) // makes queryStringParameters and pathParameters always available
   if (['staging', 'production'].includes(stage)) {
     middified.use(httpSecurityHeaders())  // Applies best practice security headers to responses.
   }

a10c76293e99a306cd207d25417163378e3aaa78
Anssi Herranen <anssi.herranen@vincit.fi>
2019-03-04 16:14:07 +0200

    Removed (and invalidated) Contentful Access token from code. Using dotenv instead.

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,10 @@
-export default {
-  contentful: {
-    accessToken: '8c1cc0a10e01375d27c4c99f8a6d0677280a9d6a440d1215659c8fc3087ec3f0',
-    space: 'i482t2ldlj3c', // space id
-  },
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const { CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_SPACE_ID } = process.env
+
+export const contentful = {
+  accessToken: CONTENTFUL_ACCESS_TOKEN,
+  space: CONTENTFUL_SPACE_ID,
 }

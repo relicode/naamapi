ec0e0f6a3b9a5113258f31bbfcb6b9de1499d09c
Anssi Herranen <anssi.herranen@vincit.fi>
2019-03-04 18:14:38 +0200

    Added trimming of contentful entries to just return required fields

--- src/services/contentful.ts
+++ src/services/contentful.ts
@@ -2,11 +2,18 @@ import { createClient } from 'contentful'
 import { Entry } from 'contentful'
 import createError from 'http-errors'
 import { contentful as contentfulConfig } from '@utils/config'
+import Logger from '@utils/logger'
+
+const { log } = new Logger('services/contentful')
 
 interface MainPageFields {
   title: string,
   headerImage: any,
   content: string,
+}
+
+interface TrimmedMainPage extends MainPageFields {
+  headerImage: string,
 }
 
 interface MainPageIdMap {
@@ -19,9 +26,23 @@ const mainPageIdMap: MainPageIdMap = {
 
 export const contentfulClient = createClient(contentfulConfig)
 
-export const fetchMainPage = <T extends keyof MainPageIdMap>(pageName: T): Promise<Entry<MainPageFields>> => {
+const trimMainPage = (entry: Entry<MainPageFields>): TrimmedMainPage => {
+  const { headerImage, title, content } = entry.fields
+  const { url } = headerImage.fields.file
+  return {
+    content,
+    headerImage: url,
+    title,
+  }
+}
+
+export const fetchMainPage = async <T extends keyof MainPageIdMap>(pageName: T): Promise<TrimmedMainPage> => {
   if (!mainPageIdMap.hasOwnProperty(pageName)) {
     throw new createError.NotFound()
   }
-  return contentfulClient.getEntry(mainPageIdMap[pageName])
+
+  const entry: Entry<MainPageFields> = await contentfulClient.getEntry(mainPageIdMap[pageName])
+  log(`Successfully fetched page: ${entry.fields.title}`)
+  const trimmedEntry = trimMainPage(entry)
+  return trimmedEntry
 }

bc4050969ee97404739828ab98bd269a0cecf602
Anssi Herranen <anssi.herranen@iki.fi>
2019-03-19 23:39:08 +0200

    Added url, height and width to headerImage

--- src/services/contentful/types.ts
+++ src/services/contentful/types.ts
@@ -9,12 +9,16 @@ export interface MainInfoPageFields {
 
 export type TrimmedMainInfoPageFields = Omit<MainInfoPageFields, 'headerImage'>
 
+export interface HeaderImage {
+  url: string,
+  width: number,
+  height: number,
+}
+
 export interface RecordFields {
   createdAt: string,
-  headerImage: string,
+  headerImage: HeaderImage,
   updatedAt: string,
 }
 
-export interface TrimmedMainInfoPageEntry extends TrimmedMainInfoPageFields, RecordFields {
-  headerImage: string,
-}
+export interface TrimmedMainInfoPageEntry extends TrimmedMainInfoPageFields, RecordFields {}

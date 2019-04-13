69329a32ca73aa33bf35a02f71347537f6b00ae4
Anssi Herranen <anssi.herranen@iki.fi>
2019-04-13 04:29:31 +0300

    Refactored the entire dynamic content fetching

--- src/services/contentful/index.ts
+++ src/services/contentful/index.ts
@@ -1,44 +1,92 @@
 import { getCacheFile } from '@services/cache'
 import { contentfulClient as client } from '@utils/contentful'
 import Logger from '@utils/logger'
-import { Entry, EntryCollection } from 'contentful'
 
-import { MainInfoPageFields, MainInfoPageResponse, TrimmedMainInfoPageEntry } from './types'
+import {
+  DynamicContentEntryCollection,
+  DynamicContentRecord,
+  DynamicContentResponse,
+  DynamicContentTypes,
+  MainInfoPageFields,
+  MainInfoPageRecord,
+  PerformerFields,
+  PerformerRecord,
+} from './types'
 
-const { log } = new Logger('services/contentful')
+const { error, log } = new Logger('services/contentful')
 
-const mainInfoPageEntriesToRecords = (entry: Entry<MainInfoPageFields>): TrimmedMainInfoPageEntry => {
-  const { createdAt, updatedAt } = entry.sys
-  const { headerImage, title, content, order } = entry.fields
+const replaceImageUrls = (content: string): string => (
+  content.replace(/\/\/images\.ctfassets\.net/g, 'https://images.ctfassets.net')
+)
 
-  return {
-    content: content.replace(/\/\/images\.ctfassets\.net/g, 'https://images.ctfassets.net'),
-    createdAt,
-    headerImage: {
-      url: headerImage.fields.file.url,
-      width: headerImage.fields.file.details.image.width,
-      height: headerImage.fields.file.details.image.height,
-    },
-    order,
-    title,
-    updatedAt,
-  }
+const imageToRecordField = (imageEntryField) => ({
+  url: imageEntryField.fields.file.url,
+  width: imageEntryField.fields.file.details.image.width,
+  height: imageEntryField.fields.file.details.image.height,
+})
+
+const convertEntriesToRecords = (entryCollection: DynamicContentEntryCollection): DynamicContentRecord[] => {
+  const { items } = entryCollection
+
+  return items.map((item) => {
+    const { createdAt, updatedAt } = item.sys
+    const recordBase = { createdAt, updatedAt }
+    const { id } = item.sys.contentType.sys
+
+    switch (id as DynamicContentTypes) {
+
+      case 'mainInfoPage':
+        const mainInfoPageFields = item.fields as MainInfoPageFields
+        return {
+          ...recordBase,
+          title: mainInfoPageFields.title,
+          content: replaceImageUrls(mainInfoPageFields.content),
+          order: mainInfoPageFields.order,
+          headerImage: imageToRecordField(mainInfoPageFields.headerImage),
+        } as MainInfoPageRecord
+
+      case 'performer':
+        const performerFields = item.fields as PerformerFields
+        return {
+          ...recordBase,
+          description: replaceImageUrls(performerFields.description),
+          headerImage: imageToRecordField(performerFields.headerImage),
+          isStar: performerFields.isStar,
+          name: performerFields.name,
+        } as PerformerRecord
+
+      default:
+        throw new Error('Invalid content type: ' + id)
+    }
+  })
 }
 
-export const fetchDynamicContent = async (contentType: string): Promise<MainInfoPageResponse> => {
-  const values = await Promise.all([
-    client.getEntries(
-      { content_type: contentType, order: 'fields.order' },
-    ) as Promise<EntryCollection<TrimmedMainInfoPageEntry>>,
-    getCacheFile(),
-  ])
-  log('Successfully fetched MainInfoPages')
+const getRecords = async (contentType: DynamicContentTypes):
+  Promise<{ [key: string]: DynamicContentRecord[] }> => {
+    log(`Getting entries for ${contentType}`)
 
-  const entries = values[0]
-  const synced = values[1].time
+    try {
+      const entryCollection = await client.getEntries({
+        content_type: contentType,
+      }) as DynamicContentEntryCollection
+      return { [contentType]: convertEntriesToRecords(entryCollection) }
+    } catch (e) {
+      error(`Error fetching ${contentType}`)
+      error(e)
+    }
+}
+
+export const fetchDynamicContent = async (contentTypes: DynamicContentTypes[]): Promise<DynamicContentResponse> => {
+  const values = await Promise.all([
+    ...contentTypes.map((contentType) => getRecords(contentType)),
+  ])
+
+  const cacheFile = await getCacheFile()
+
+  log(`Successfully fetched ${contentTypes.join(', ')}`)
 
   return {
-    mainInfoPages: entries.items.map((entry) => mainInfoPageEntriesToRecords(entry)),
-    synced,
+    ...values.splice(0, 2),
+    synced: cacheFile.time,
   }
 }

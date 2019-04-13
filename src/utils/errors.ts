b3c5e14f4d495a39f92913ccbab37b7f9e3686b7
Anssi Herranen <anssi.herranen@iki.fi>
2019-04-13 04:29:31 +0300

    Refactored the entire dynamic content fetching

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -17,37 +17,3 @@ export const createErrorResponse = (error: LambdaError): APIGatewayProxyResult =
   statusCode: error.statusCode,
 })
 
-export function errorWrapper(handler: APIGatewayProxyHandler) {
-  return function(...args: [APIGatewayEvent, Context?, Callback<APIGatewayProxyCallback>?]): APIGatewayProxyResult {
-    try {
-      handler.call(this, args)
-    } catch (e) {
-      if (e.constructor.super_ && e.constructor.super_.name === 'HttpError') {
-        return createErrorResponse(e)
-      }
-      return createErrorResponse({
-        statusCode: 500,
-        message: e.message,
-      })
-    }
-  }
-}
-
-/*
-export const errorWrapper = (
-  handler: APIGatewayProxyHandler,
-  ...rest: [APIGatewayEvent, Context?, Callback<APIGatewayProxyCallback>?]
-): APIGatewayProxyResult => {
-  try {
-    return handler.call(...rest)
-  } catch (e) {
-    if (e.constructor.super_ && e.constructor.super_.name === 'HttpError') {
-      return createErrorResponse(e)
-    }
-    return createErrorResponse({
-      statusCode: 500,
-      message: e.message,
-    })
-  }
-}
-*/
